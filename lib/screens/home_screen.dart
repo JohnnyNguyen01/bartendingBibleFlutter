2c21d2128d13574eff99196f9e6093b1ed1a5964
johnny <johnnynguyenwork@gmail.com>
2020-03-17 21:41:59 +1100

    Started home page ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,72 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: SafeArea(
+        top: false,
+        child: Column(
+          children: <Widget>[
+            Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                //image container - background image
+                Container(
+                  width: 500,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image:
+                            AssetImage('lib/images/brooke-lark-unsplash.png')),
+                  ),
+                ),
+                // Logo widget
+                Positioned(
+                  bottom: 60,
+                  left: 130,
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('lib/images/bar-app-logo.png')),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 40,
+                  left: 85,
+                  child: Container(
+                      color: Colors.black,
+                      child: Text(
+                        'Cocktails at your fingertips',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                ),
+                //Search bar card
+                Positioned(
+                  bottom: -35,
+                  left: 50,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)),
+                    child: Container(
+                      padding: EdgeInsets.only(left:10),
+                      width: 300,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.search),
+                            border: InputBorder.none,
+                            hintText: 'Find Cocktail'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
